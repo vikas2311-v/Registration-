ed313b09a1abda13e437094526b1a05c2e7e88e8
vikas2311-v <72648410+vikas2311-v@users.noreply.github.com>
2024-04-03 23:13:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function white() {
 
 function black() {
     var pass = document.getElementById("password").value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
 
     if (pass.match(pattern)) {
         alertp.innerHTML = "** White Spaces are not allowed in Password";
@@ -37,6 +38,44 @@ function black() {
     return true;
 }
 
+function validatePassword() {
+    var pass = document.getElementById("password").value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (pass !== confirmPassword) {
+        alertp.innerHTML = "** Passwords do not match";
+        return false;
+    }
+
+    alertp.innerHTML = "";
+    return true;
+}
+
+function populateStates() {
+    var countrySelect = document.getElementById("country");
+    var stateSelect = document.getElementById("state");
+    var country = countrySelect.options[countrySelect.selectedIndex].value;
+
+    stateSelect.innerHTML = "<option value=''>Select State</option>";
+    if (country === "India") {
+        stateSelect.innerHTML += "<option value='state1'>Karnataka</option>";
+        stateSelect.innerHTML += "<option value='state2'>Maharashtra</option>";
+        stateSelect.innerHTML += "<option value='state3'>Andhra Pradesh</option>";
+    } else if (country === "China") {
+        stateSelect.innerHTML += "<option value='state4'>Guangxi</option>";
+        stateSelect.innerHTML += "<option value='state5'>Ningxia</option>";
+        stateSelect.innerHTML += "<option value='state6'>Hebei</option>";
+    } else if (country === "Korea") {
+        stateSelect.innerHTML += "<option value='state7'>North Chung cheong</option>";
+        stateSelect.innerHTML += "<option value='state8'>SouthChung cheong</option>";
+        stateSelect.innerHTML += "<option value='state9'>Gangwon</option>";
+    } else if (country === "Japan") {
+        stateSelect.innerHTML += "<option value='state10'> Akita</option>";
+        stateSelect.innerHTML += "<option value='state11'> Yamagata</option>";
+        stateSelect.innerHTML += "<option value='state12'> Fukushima</option>";
+    }
+}
+
 function space(data) {
     var isSpace = pattern.test(data);
     if (isSpace) {
@@ -46,25 +85,6 @@ function space(data) {
     return true;
 }
 
-function validateForm() {
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('confirmPassword').value;
-
-    if (password !== confirmPassword) {
-        alert("Passwords do not match!");
-        return false;
-    }
-
-    return true;
-}
-
-function validateForm() {
-    if (!white() || !black()) {
-        alertp.innerHTML = "* Please fill all details";
-        return false;
-    }
-    return true;
-}
 
 function submitForm(event) {
     event.preventDefault();
