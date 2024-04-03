13c4f45b3566ab2574a8090ced22da11a7b74486
vikas2311-v <72648410+vikas2311-v@users.noreply.github.com>
2024-04-03 17:52:38 +0530

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,62 @@
+var pattern = /\s/g;
+var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
+var passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+var alertp = document.getElementById('alert');
+
+function white() {
+    var user = document.getElementById("email").value;
+
+    if (user.match(pattern)) {
+        alertp.innerHTML = "** White Spaces are not allowed in Email";
+        return false;
+    }
+
+    if (!user.match(emailPattern)) {
+        alertp.innerHTML = "** Invalid Email Format";
+        return false;
+    }
+
+    alertp.innerHTML = "";
+    return true;
+}
+
+function black() {
+    var pass = document.getElementById("password").value;
+
+    if (pass.match(pattern)) {
+        alertp.innerHTML = "** White Spaces are not allowed in Password";
+        return false;
+    }
+
+    if (!pass.match(passwordPattern)) {
+        alertp.innerHTML = "* Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character";
+        return false;
+    }
+
+    alertp.innerHTML = "";
+    return true;
+}
+
+function space(data) {
+    var isSpace = pattern.test(data);
+    if (isSpace) {
+        alertp.innerHTML = "** White Spaces are not allowed in Email";
+        return false;
+    }
+    return true;
+}
+
+function validateForm() {
+    if (!white() || !black()) {
+        alertp.innerHTML = "* Please fill all details";
+        return false;
+    }
+    return true;
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    if (validateForm()) {
+        window.location.href = "welcome.html";
+    }
+}
